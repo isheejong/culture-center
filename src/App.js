98839396001b7a14e6f9724bd29ddda8ac835359
leehee <leehee@amazon.com>
2020-09-02 00:08:27 +0900

    practice component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,49 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+class Article extends Component{
+  render () {
+    return (
+        <article>
+          <h2>HTML</h2>
+           HTML is HyperText Markup Lanuage.App
+       </article>
+    );
+  }
+}
+class Nav extends Component {
+
+  render(){
+    return(
+        <nav>
+          <ul>
+              <li><a href="1.html">HTML</a></li>
+              <li><a href="2.html">CSS</a></li>
+              <li><a href="3.html">JavaScript</a></li>
+          </ul>
+        </nav>
+    );
+  }
+}
+
+class Subject extends Component {
+  render() {
+    return(
+      <header>
+          <h1>WEB</h1>
+          world wide web!
+      </header>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, Emart Culture Center
+        <Subject></Subject>
+        <Nav></Nav>
+        <Article></Article>
       </div>
     )
   }
